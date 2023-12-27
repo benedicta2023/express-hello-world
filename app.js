4490b92659d28852e1450859d19a63b886e4b601
benedicta2023 <154936980+benedicta2023@users.noreply.github.com>
2023-12-27 08:51:04 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,43 +1,8 @@
-const express = require('express')
-const path = require("path");
-const app = express()
+<form action="https://formbold.com/s/91EE0" method="POST">
+  <input type="email" placeholder="Email" name="email">
+  <input type="text" placeholder="Subject" name="subject">
+  <input type="file" placeholder="File" name="file">
+  <textarea name="message" placeholder="Type your message"></textarea>
 
-// #############################################################################
-// Logs all request paths and method
-app.use(function (req, res, next) {
-  res.set('x-timestamp', Date.now())
-  res.set('x-powered-by', 'cyclic.sh')
-  console.log(`[${new Date().toISOString()}] ${req.ip} ${req.method} ${req.path}`);
-  next();
-});
-
-// #############################################################################
-// This configures static hosting for files in /public that have the extensions
-// listed in the array.
-var options = {
-  dotfiles: 'ignore',
-  etag: false,
-  extensions: ['htm', 'html','css','js','ico','jpg','jpeg','png','svg'],
-  index: ['index.html'],
-  maxAge: '1m',
-  redirect: false
-}
-app.use(express.static('public', options))
-
-// #############################################################################
-// Catch all handler for all other request.
-app.use('*', (req,res) => {
-  res.json({
-      at: new Date().toISOString(),
-      method: req.method,
-      hostname: req.hostname,
-      ip: req.ip,
-      query: req.query,
-      headers: req.headers,
-      cookies: req.cookies,
-      params: req.params
-    })
-    .end()
-})
-
-module.exports = app
+  <button type="submit">Send Message</button>
+</form>
